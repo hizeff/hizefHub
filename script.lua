074f420a4a2807c7a54459a7fa11ad69983d86ac
hizef <likecountdimon@gmail.com>
2026-07-16 03:54:02 +0300

    script.lua

---
+++ script.lua
@@ -0,0 +1,11 @@
+--[[
+
+  The script hasn't been created yet. it will be created later.
+
+]]
+while true do
+warn("The script hasn't been created yet. it will be created later.")
+warn("The script hasn't been created yet. it will be created later.")
+warn("The script hasn't been created yet. it will be created later.")
+wait(0.1)
+end
